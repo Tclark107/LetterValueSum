525bcb89aa7ab5e29671f2901a6a4bc1f71aef4c
Tristan Clark <tcjc107@gmail.com>
2023-09-18 20:55:14 -0400

    I lost my progress, here is some progress

--- LetterValueSum.cpp
+++ LetterValueSum.cpp
@@ -1,1 +1,59 @@
-//I'm an idiot
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> words;
+std::vector<int> amountOfTimes;
+
+enum alphabet
+{
+    empty = 0,
+    a = 1,
+    b = 2,
+    c = 3,
+    d = 4,
+    e = 5,
+    f = 6,
+    g = 7,
+    h = 8,
+    i = 9,
+    j = 10,
+    k = 11,
+    l = 12,
+    m = 13,
+    n = 14,
+    o = 15,
+    p = 16,
+    q = 17,
+    r = 18,
+    s = 19,
+    t = 20,
+    u = 21,
+    v = 22,
+    w = 23,
+    x = 24,
+    y = 25,
+    z = 26
+};
+
+void saveInput()
+{
+    std::ifstream myfile;
+    myfile.open("Input.txt");
+    if(myfile.is_open())
+    {
+        std::string line;
+        while(getline(myfile,line))
+        {
+            words.push_back(line);
+        }
+
+    }
+    myfile.close();
+}
+
+int main()
+{
+    saveInput();
+}
